b00aacdad4ebba56c7c829974eb4e523016c52ef
Wale <okemuyiwaseun@gmail.com>
2022-01-01 14:12:12 +0100

    vibes

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,25 +1,64 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);  //isLogin is state whether we're about to log in or not
+  const [isLogin, setIsLogin] = useState(true);  //isLogin is state which true signifies about to Log in and false signifies about to sign up
+
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
 
   const toggleAuthModeHandler = () => {
     setIsLogin(latestState => !latestState);
   };
 
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    //Optional validation for email and password
+
+    if(isLogin) {
+
+    } else {
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC10myv-0cDjhwdwT_i6T8URC3xUxfw6x8', {
+        method: 'POST',
+        body: JSON.stringify({ 
+          email: enteredEmail, 
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          //DO STH
+        } else {
+          response.json().then(data => {
+            //SHOW ERROR MODAL
+            console.log('response not okay-', data.error.message);
+          });
+        }
+      })
+      
+      
+
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' ref={emailInputRef} id='email' required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' ref={passwordInputRef} id='password' required />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
