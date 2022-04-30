d801da8bccda4af438df0c340845c236385c12b6
Wale <okemuyiwaseun@gmail.com>
2022-04-30 14:00:49 +0100

    changed somethings

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,25 +1,25 @@
-import { useContext, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import AuthContext from '../../store/auth-context';
+import { useContext, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../store/auth-context";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);  //isLogin is state which signifies if user is about to Log in or not(therefore creating an account)
+  const [isLogin, setIsLogin] = useState(true); //isLogin is state which signifies if user is about to Log in or not(therefore creating an account)
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
 
   const authCtx = useContext(AuthContext);
-  
+
   const navigate = useNavigate();
 
   const toggleAuthModeHandler = () => {
     setIsLogin(latestState => !latestState);
   };
 
-  const submitHandler = async (event) => {
+  const submitHandler = async event => {
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
@@ -27,70 +27,88 @@ const AuthForm = () => {
     //Optional validation for email and password
 
     setIsLoading(true);
+    const { idToken, expiresIn } = {
+      idToken: "somebullshittoken123",
+      expiresIn: "1800",
+    };
+    const expirationTimestamp =
+      new Date().getTime() + parseInt(expiresIn, 10) * 1000;
+    authCtx.login(idToken, expirationTimestamp);
+    navigate("/", { replace: true });
 
     let url;
-    if(isLogin) {  //if Login or signup (the same logic applies to both just dfferent URLs)
-      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC10myv-0cDjhwdwT_i6T8URC3xUxfw6x8';
+    if (isLogin) {
+      //if Login or signup (the same logic applies to both just dfferent URLs)
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC10myv-0cDjhwdwT_i6T8URC3xUxfw6x8";
     } else {
-      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC10myv-0cDjhwdwT_i6T8URC3xUxfw6x8';
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC10myv-0cDjhwdwT_i6T8URC3xUxfw6x8";
     }
 
     try {
       const response = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({ 
-          email: enteredEmail, 
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
           password: enteredPassword,
-          returnSecureToken: true
+          returnSecureToken: true,
         }),
         headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-  
+          "Content-Type": "application/json",
+        },
+      });
+
       setIsLoading(false);
-      
-      const data = await response.json();  
-      const { idToken, expiresIn } = data;      //expiresIn is the time the login section expires as specified by firebase which is '3600' or 60mins or 1hr 
-      
-      if(response.ok) {      
-        const expirationTimestamp = new Date().getTime() + (parseInt(expiresIn) * 1000);
-        const expirationTimeDateString = new Date(expirationTimestamp).toISOString();  
-        authCtx.login(idToken, expirationTimeDateString);
-        navigate('/', {replace: true});
+
+      const data = await response.json();
+      const { idToken, expiresIn } = data; //expiresIn is the time the login section expires as specified by firebase which is '3600' or 60mins or 1hr
+
+      if (response.ok) {
+        const expirationTimestamp =
+          new Date().getTime() + parseInt(expiresIn, 10) * 1000;
+
+        authCtx.login(idToken, expirationTimestamp);
+
+        navigate("/", { replace: true });
       } else {
-        let errorMessage = 'Authentication failed';
+        let errorMessage = "Authentication failed";
         errorMessage = data.error.message ? data.error.message : errorMessage;
         throw new Error(errorMessage);
       }
-      
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
-    
-  }
+  };
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' ref={emailInputRef} id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" ref={emailInputRef} id="email" required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' ref={passwordInputRef} id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            ref={passwordInputRef}
+            id="password"
+            required
+          />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
           {isLoading && <p>Sending request...</p>}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={toggleAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,40 +1,39 @@
 import React, { useCallback, useEffect, useState } from "react";
 
-export const AuthContext = React.createContext({
-  token: '',
+const AuthContext = React.createContext({
+  token: "",
   isLoggedIn: false,
   login: token => {},
-  logout: () => {}
+  logout: () => {},
 });
 
+const calRemainingTime = expirationTimestamp => {
+  const currentTimestamp = new Date().getTime();
+  const timeLeft = expirationTimestamp - currentTimestamp; //in milliseconds
+  return timeLeft;
+};
 
-
-const calRemainingTime = expirationTime => {
-  const currentTimestamp = new Date().getTime();                                       
-  const expirationTimestamp = new Date(expirationTime).getTime();     
-  return expirationTimestamp - currentTimestamp;    //in milliseconds
-}
-
-let logoutTimer; 
+let logoutTimer;
 
 const retrieveStoredToken = () => {
-  const storedToken = localStorage.getItem('token');
-  const storedExpirationDate = localStorage.getItem('expirationTime');
+  const storedToken = localStorage.getItem("token");
+  const storedExpirationTimestamp = localStorage.getItem("expirationTimestamp");
 
-  const remainingTime = calRemainingTime(storedExpirationDate);  
+  const remainingTime = calRemainingTime(storedExpirationTimestamp);
 
-  if (remainingTime <= 60000) {             //if the remaining time for the logIn status to expire is 1 mins or less than that (so no need to automatically log user in as time is almost up instead the user should log in manually)
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationTime')
+  if (remainingTime <= 60000) {
+    //if the remaining time for the logIn status to expire is 1 mins or less than that (so no need to automatically log user in as time is almost up instead the user should log in manually)
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTimestamp");
     return null;
   }
 
   //but if the remaining time is more than a minute(> 60s), then the user token can be retrieved and should be automatically logged In cause there is enough time left
   return {
     token: storedToken,
-    duration: remainingTime
-  }
-}
+    duration: remainingTime,
+  };
+};
 
 export const AuthContextProvider = props => {
   const tokenData = retrieveStoredToken();
@@ -43,45 +42,45 @@ export const AuthContextProvider = props => {
     initialToken = tokenData.token;
     //logoutTimer would have been implemented here but since logoutHandler can't be accessed here it is set in useEffect down after logoutHandler has been initialized.
   }
-  const [token, setToken] = useState(initialToken); 
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = Boolean(token);
 
   const logoutHandler = useCallback(() => {
     setToken(null);
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationTime');
+    localStorage.removeItem("token");
+    localStorage.removeItem("expirationTimestamp");
 
-    if(logoutTimer) clearTimeout(logoutTimer);
+    if (logoutTimer) clearTimeout(logoutTimer);
   }, []);
 
-  const loginHandler = (token, expirationTime) => {
+  const loginHandler = (token, expirationTimestamp) => {
     setToken(token);
-    localStorage.setItem('token', token);
-    localStorage.setItem('expirationTime', expirationTime)
+    localStorage.setItem("token", token);
+    localStorage.setItem("expirationTimestamp", expirationTimestamp);
 
-    const remainingTime = calRemainingTime(expirationTime);
-    logoutTimer = setTimeout(logoutHandler, remainingTime);     //after the remaining time(3600s/60mins/1hr i.e when sign In expires) automatically log user out
+    const remainingTime = calRemainingTime(expirationTimestamp);
+    logoutTimer = setTimeout(logoutHandler, remainingTime); //after the remaining time(3600s/60mins/1hr i.e when sign In expires) automatically log user out
   };
 
   useEffect(() => {
     if (tokenData) {
-      logoutTimer = setTimeout(logoutHandler, tokenData.duration);    //after the user is Logged in automatically, then also log the user out automatically when his or her time left is exhausted
+      logoutTimer = setTimeout(logoutHandler, tokenData.duration); //after the user is Logged in automatically, then also log the user out automatically when his or her time left is exhausted
     }
-  }, [tokenData, logoutHandler])
+  }, [tokenData, logoutHandler]);
 
   const authContextData = {
     token: token,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
-    logout: logoutHandler
-  }
+    logout: logoutHandler,
+  };
 
   return (
     <AuthContext.Provider value={authContextData}>
       {props.children}
     </AuthContext.Provider>
-  )
+  );
 };
 
 export default AuthContext;
